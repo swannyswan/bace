28cf0cace9a235742badcd601529cf8de3c7bffe
swannyswan <71267505+swannyswan@users.noreply.github.com>
2022-04-03 21:22:19 -0400

    refactor: update theta_4 max (5 --> 20)

--- database_scripts/db_user.sql
+++ database_scripts/db_user.sql
@@ -16,8 +16,8 @@ SELECT gen_grid_thetas(theta => ARRAY[
     [-10, 10, 1], -- Parameter 1 values will be: [-20, -19, ..., 19, 20]
     [-10, 10, 1], -- Parameter 2 values will be: [-5, -4, ..., 4, 5]
     [-5, 5, 1], -- Parameter 3 values will be: [-1.5, -1, -0.5, 0, 0.5, 1, 1.5],
-    [1, 5, 2] -- Parameter 4 values will be: [1, 3, 5]
-]);
+    [1, 20, 2] -- Parameter 4 values will be: [1, 3, 5]
+    );
 
 /*
     STEP 2: CREATE Designs - Grid of possible discrete choice designs that can be asked
--- user_modules/user_defaults.js
+++ user_modules/user_defaults.js
@@ -8,7 +8,7 @@ const allow_repeated_designs = Boolean(false); // true if yes, false if no
 const default_sample_percentage_theta = 100;
 
 // Percentage of question designs to randomly search over when choosing the optimal design each round. Default 100 - All rows
-const default_sample_percentage_designs = 60;
+const default_sample_percentage_designs = 25;
 
 // Export Functions to be used in other files.
 module.exports = {
