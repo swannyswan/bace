73e63830b3d78daf6fc83f3dfe05ec83f0fbb885
swannyswan <71267505+swannyswan@users.noreply.github.com>
2022-04-10 14:08:27 -0400

    test: 15,20,25,30

--- user_modules/characteristics.js
+++ user_modules/characteristics.js
@@ -4,9 +4,9 @@ const example_payment_scheme = "daily"; // Example payment_scheme for test
 const treated_survey_value = 1; // Recode value in Qualtrics
 
 // for testing
-const theta_1 = 5;
-const theta_2 = 10;
-const theta_3 = 15;
+const theta_1 = 15;
+const theta_2 = 20;
+const theta_3 = 25;
 const theta_4 = 30;
 
 const characteristics = {
